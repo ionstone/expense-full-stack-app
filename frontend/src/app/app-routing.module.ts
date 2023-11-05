68f774e2f0721025fc38fcf9a3207daaf8d21871
Your Name <visheshyadav2001@gmail.com>
2023-11-06 01:04:20 +0530

    add user name

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './app/auth.guard';
 
 
 
@@ -10,10 +11,11 @@ const routes: Routes = [
 { path: '', redirectTo: '/login', pathMatch: 'full'},
 
   {path:'login',component:LoginComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 
     {path:'register',component:RegistrationComponent},
 
-  {path:'dashboard',component:DashboardComponent}
+  // {path:'dashboard',component:DashboardComponent}
 
 ];
 
